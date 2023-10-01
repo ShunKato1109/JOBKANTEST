4495c7fb50a406312c9ede4784b268d6ca3713d8
Shunsuke <shunliangj@gmai.com>
2023-10-01 21:54:06 +0900

    ヘッダー2たたき台完成-231001

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,39 @@ import './App.css';
 
 export const App = () =>{
 
-  const StyleDiv = styled.div`
-  display: flex;
-  justify-content: space-between; 
-  align-items: left;
-  width: 100%;
-  
+  const StyledHead1 = styled.header`
+  background-color:#222222;
+  display:flex;
+  align-items:center;
+  color:white;
+  height:25px;
   `;
 
-  const StyledHead1 = styled.header`
-  background-color:black;
-  color:white;
-  `;
+  const HeadButton =(props)=>{
+    const headButtonStyle={
+      color:'white',
+      backgroundColor:'transparent',
+      fontSize:'50%',
+      paddingLeft:'25px',
+      paddingRight:'25px',
+      cursor:'pointer',
+      border:'none',
+      
+    };
+    return <button style={headButtonStyle}>{props.label}</button>
+  }
 
   const StyledHead2 = styled.header`
   background-color:#30B2D6;
   color:white;
+  display: flex;
+  align-items:center;
+  height:80px;
+  `;
+
+  const ContainerDiv = styled.div`
+  display: flex;
+  align-items: baseline;
   `;
 
 
@@ -27,23 +44,24 @@ export const App = () =>{
     <>
     {/* ヘッダー１　ドロップダウンあり */}
     <StyledHead1>
-      <div>
-        <p>ヘッダー１</p>
+      <div style={{display:'flex',alignItems:'center'}}>
+        <p style={{padding:'10px'}}>ジョブカン</p>
+        <HeadButton label="勤怠"></HeadButton>
+        <HeadButton label="労務"></HeadButton>
+        <HeadButton label="給与"></HeadButton>
       </div>
     </StyledHead1>
 
     {/* ヘッダー２　ジョブカン */}
     <StyledHead2>
-      <StyleDiv>
-        <div className="container">
-        <p style={{fontSize:'36px'}}>ジョブカン</p>
-        <p style={{fontSize:'18px'}}>勤怠管理</p>
+        <ContainerDiv>
+        <p style={{fontSize:'200%',fontWeight:'bold'}}>ジョブカン</p>
+        <p style={{fontSize:'50%'}}>勤怠管理</p>
+        </ContainerDiv>
+        <div style={{marginLeft:'auto',marginRight:'20px',textDecoration:'underline',lineHeight:'1'}}>
+        <p style={{fontSize:'25%',}}>加藤舜涼さん</p>
+        <p style={{fontSize:'25%'}}>スタッフコード(rv230009)</p>
         </div>
-        <div className="right-item">
-        <p >加藤舜涼さん</p>
-        <p >スタッフコード</p>
-        </div>
-      </StyleDiv>
     </StyledHead2>
 
     {/* サイドバー */}
