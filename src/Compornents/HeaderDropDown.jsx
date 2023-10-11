c53ba3f9f899a8ba846404586722b6482612bba9
Shunsuke <shunliangj@gmai.com>
2023-10-11 22:04:52 +0900

    ヘッダー１レイアウトたたき台完成(231011)

--- src/Compornents/HeaderDropDown.jsx
+++ src/Compornents/HeaderDropDown.jsx
@@ -3,38 +3,39 @@ import styled from "styled-components";
 import { useState } from "react";
 
 export const SdropDownDiv = styled.div`
+margin-left:auto;
 position:relative;
 display:inline-block;
 `;
 
 export const SdropDownButton = styled.button`
-color:white;
-border:none;
-display:flex;
-font-size:10px;
-    margin-left:auto;
-    padding-left:10px;
-    padding-right:10px;
-    cursor:pointer;
+    color:#c0c0c0;
+    border:none;
+    font-size:10px;
+    padding-right:20px;
     height:100%;
-    border-left:0.5px solid gray;
-
-background-color:transparent;
-cursor:pointer`;
+    background-color:transparent;
+    cursor:pointer
+`;
 
 const SdropdownContent = styled.div`
   display: ${props => (props.show ? 'block' : 'none')};
+  font-size:12px;
   position: absolute;
   background-color: #f9f9f9;
-  min-width: 160px;
+  min-width: 230px;
   box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-  z-index: 1;
+  right:10px;
+  border-bottom-left-radius:5px;
+  border-bottom-right-radius:5px;
 
   a {
-    color: black;
+    color: #c1;
     padding: 12px 16px;
     text-decoration: none;
     display: block;
+    height:15px;
+
   }
 
   a:hover {
@@ -42,24 +43,12 @@ const SdropdownContent = styled.div`
   }
 `;
 
-
-  //Header button3 css
-  export const SdropDown={
-    fontSize:'10px',
-    marginLeft:'auto',
-    paddingLeft:'10px',
-    paddingRight:'10px',
-    cursor:'pointer',
-    height:'100%',
-    borderLeft:'0.5px solid gray',
-  };
-
 export const HeaderDropDown = ()=>{
     const[show,setShow] = useState(false);
 
     return (
         <SdropDownDiv>
-            <SdropDownButton style={{SdropDown}} onClick={()=>setShow(!show)}>アカウント</SdropDownButton>
+            <SdropDownButton onClick={()=>setShow(!show)}>アカウント</SdropDownButton>
             <SdropdownContent show={show}>
                 <a href="#">パスワード変更</a>
                 <a href="#">ログアウト</a>
